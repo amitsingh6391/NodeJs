a0546d0d3cab79e23eccad12c0319e25e483c61d
Frontend Partner <frontend.app@kuhoo.com>
2023-08-07 00:38:09 +0530

    add get with mysql

--- mysql/index.js
+++ mysql/index.js
@@ -1,18 +1,75 @@
 const mysql = require('mysql');
+const express = require('express');
 
-const con = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'test'
+const con = require('./config');
+
+const app = express();
+
+
+
+
+// const con = mysql.createConnection({
+//     host: 'localhost',
+//     user: 'root',
+//     password: '',
+//     database: 'test'
+// });
+
+// con.connect((err) => {
+
+//     if (err) {
+//         console.log('error');
+//     } else {
+
+//         console.log('connected');
+//     }
+
+// });
+
+// con.query("select * from users", (err, result) => {
+
+//     console.log("result", result)
+// })
+
+
+app.get('/', (req, res) => {
+
+    con.query("select * from users", (err, result) => {
+        if (err) {
+
+            res.send('error');
+
+        } else {
+
+            res.send(result);
+
+        }
+
+    });
+
 });
 
-con.connect((err) => {
+app.post('/', (req, res) => {
 
-    if (err) {
-        console.log('error');
-    } else {
+    const data = {
 
-    } console.log('connect');
+        name: "Amit Singh",
+        password: "Singh@639134",
+        users_type: "visitor"
+    }
+
+
+    con.query("INsert INTO users SET ?", data, (error, result, fields) => {
+
+        if (error) {
+
+            res.send(error);
+
+        } else {
+            res.send(result);
+        }
+    })
 
 });
+
+app.listen(4000);
